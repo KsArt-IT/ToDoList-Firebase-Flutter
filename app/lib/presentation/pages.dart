742a308f3f0061b4d4001c8df3586701e8a48bcb
KsArT-IT <ksart.it@gmail.com>
2025-04-12 22:27:19 +0300

    feat: add pages.dart file to export app pages

---
+++ app/lib/presentation/pages.dart
@@ -0,0 +1,8 @@
+export 'package:todo_list/presentation/pages/home/home_page.dart';
+export 'package:todo_list/presentation/pages/splash/splash_page.dart';
+export 'package:todo_list/presentation/pages/login/login_page.dart';
+export 'package:todo_list/presentation/pages/registration/registration_page.dart';
+export 'package:todo_list/presentation/pages/reset_password/reset_password_page.dart';
+export 'package:todo_list/presentation/pages/edit/edit_page.dart';
+export 'package:todo_list/presentation/pages/profile/profile_page.dart';
+export 'package:todo_list/presentation/pages/settings/settings_page.dart';
