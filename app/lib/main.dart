ab5a4c95ac8b10ddfe63559533db4bf8a700c115
KsArT-IT <ksart.it@gmail.com>
2025-04-12 22:08:56 +0300

    feat: initialize BLoC and add AppBlocObserver to main.dart

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,11 +1,14 @@
-import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:todo_list/app/app.dart';
+import 'package:todo_list/app/app_bloc_observer.dart';
 
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  Bloc.observer = const AppBlocObserver();
   runApp(const App());
 }
