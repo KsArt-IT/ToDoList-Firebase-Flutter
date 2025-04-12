541399e1e7cf42860cd685f392ce46fefda6651a
KsArT-IT <ksart.it@gmail.com>
2025-04-12 22:33:02 +0300

    feat: integration of MultiRepositoryProvider for authentication management

--- app/lib/app/app.dart
+++ app/lib/app/app.dart
@@ -1,19 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:todo_list/presentation/pages/home/home_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_list/app/app_page.dart';
+import 'package:todo_list/app/bloc/auth/auth_bloc.dart';
+import 'package:todo_list/core/router/app_router.dart';
+import 'package:todo_list/data/repositories/auth/firebase_auth_repository.dart';
+import 'package:todo_list/domain/usecases/auth_use_cases.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider(create: (_) => AppRouter()),
+        RepositoryProvider(create: (_) => FirebaseAuthRepository()),
+      ],
+      child: BlocProvider(
+        lazy: false,
+        create: (context) {
+          final authRepository = context.read<FirebaseAuthRepository>();
+          return AuthBloc(
+            signInWithEmailAndPasswordUseCase:
+                SignInWithEmailAndPasswordUseCase(authRepository),
+            signUpWithEmailAndPasswordUseCase:
+                SignUpWithEmailAndPasswordUseCase(authRepository),
+            signInWithGoogleUseCase: SignInWithGoogleUseCase(authRepository),
+            signOutUseCase: SignOutUseCase(authRepository),
+            sendPasswordResetEmailUseCase: SendPasswordResetEmailUseCase(
+              authRepository,
+            ),
+            getCurrentUserUseCase: GetCurrentUserUseCase(authRepository),
+            checkIsSignInUseCase: CheckIsSignInUseCase(authRepository),
+          )..add(CheckIsSignInEvent());
+        },
+        child: const AppPage(),
       ),
-      home: const HomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
