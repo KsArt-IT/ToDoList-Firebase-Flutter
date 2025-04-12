db5faca6164783935cca2ff3c5a5253e800e2241
KsArT-IT <ksart.it@gmail.com>
2025-04-12 22:31:25 +0300

    feat: add AppBlocObserver to monitor BLoC state

---
+++ app/lib/app/app_bloc_observer.dart
@@ -0,0 +1,41 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/foundation.dart';
+
+class AppBlocObserver extends BlocObserver {
+  const AppBlocObserver();
+
+  @override
+  void onCreate(BlocBase<dynamic> bloc) {
+    super.onCreate(bloc);
+    debugPrint('onCreate -- bloc: ${bloc.runtimeType}');
+  }
+
+  @override
+  void onEvent(Bloc<dynamic, dynamic> bloc, Object? event) {
+    super.onEvent(bloc, event);
+    debugPrint('onEvent -- bloc: ${bloc.runtimeType}, event: $event');
+  }
+
+  @override
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+    super.onChange(bloc, change);
+    debugPrint('onChange -- bloc: ${bloc.runtimeType}, event: $change');
+  }
+
+  @override
+  void onTransition(
+    Bloc<dynamic, dynamic> bloc,
+    Transition<dynamic, dynamic> transition,
+  ) {
+    super.onTransition(bloc, transition);
+    debugPrint(
+      'onTransition -- bloc: ${bloc.runtimeType}, transition: $transition',
+    );
+  }
+
+  @override
+  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
+    debugPrint('onError -- bloc: ${bloc.runtimeType}, error: $error');
+    super.onError(bloc, error, stackTrace);
+  }
+}
